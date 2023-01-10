9eacc5b51e1299c3935de61c39a08ecf441c6642
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2023-01-10 01:11:45 -0500

    Rolled game setup into setupGame(), and call when resetting

--- script.js
+++ script.js
@@ -65,63 +65,9 @@ const speakWord = (letter, el) => {
     el.remove();
     placedLettters.delete(el);
 
-    if (placedLettters.size === 0) console.log("Restart game");
-  }, 1500);
+    if (placedLettters.size === 0) setupGame();
+  }, 1);
 };
-
-alphabet.forEach((letter) => {
-  const newLetter = document.createElement("h1");
-  newLetter.innerText = letter;
-
-  newLetter.value = letter.charAt(0).toLocaleLowerCase();
-  newLetter.addEventListener("click", (e) => {
-    if (speaking) return;
-    speakWord(e.target.value, e.target);
-  });
-
-  pg.insertAdjacentElement("afterbegin", newLetter);
-
-  const randomLoc = () => {
-    let xloc = Math.floor(Math.random() * pg.offsetWidth);
-    let yloc = Math.floor(Math.random() * pg.offsetHeight);
-
-    return { xloc: xloc, yloc: yloc };
-  };
-
-  // We give up repositioning recursively after 100 recursions / failed
-  // placement attempts within setRandomLoc() and checkIntersections()
-  let recursions = 0;
-
-  const setRandomLoc = () => {
-    if (recursions >= 100) return;
-    coords = randomLoc();
-    newLetter.style.cssText = `font-size: ${fontSize}px; top: ${coords.yloc}px; left: ${coords.xloc}px`;
-
-    if (
-      coords.xloc + newLetter.offsetWidth >= pg.offsetWidth ||
-      coords.yloc + newLetter.offsetWidth >= pg.offsetHeight
-    ) {
-      setRandomLoc();
-      recursions++;
-    }
-  };
-  setRandomLoc();
-
-  const checkIntersections = () => {
-    if (recursions >= 100) return;
-
-    placedLettters.forEach((element) => {
-      if (elementsIntersect(newLetter, element)) {
-        setRandomLoc();
-        checkIntersections();
-        recursions++;
-      }
-    });
-  };
-  checkIntersections();
-
-  placedLettters.add(newLetter);
-});
 
 // Thank you chatGPT >.>
 function elementsIntersect(el1, el2) {
@@ -137,3 +83,62 @@ function elementsIntersect(el1, el2) {
     rect1.bottom > rect2.top
   );
 }
+
+const setupGame = () => {
+  placedLettters = new Set();
+  alphabet.forEach((letter) => {
+    const newLetter = document.createElement("h1");
+    newLetter.innerText = letter;
+
+    newLetter.value = letter.charAt(0).toLocaleLowerCase();
+    newLetter.addEventListener("click", (e) => {
+      if (speaking) return;
+      speakWord(e.target.value, e.target);
+    });
+
+    pg.insertAdjacentElement("afterbegin", newLetter);
+
+    const randomLoc = () => {
+      let xloc = Math.floor(Math.random() * pg.offsetWidth);
+      let yloc = Math.floor(Math.random() * pg.offsetHeight);
+
+      return { xloc: xloc, yloc: yloc };
+    };
+
+    // We give up repositioning recursively after 100 recursions / failed
+    // placement attempts within setRandomLoc() and checkIntersections()
+    let recursions = 0;
+
+    const setRandomLoc = () => {
+      if (recursions >= 100) return;
+      coords = randomLoc();
+      newLetter.style.cssText = `font-size: ${fontSize}px; top: ${coords.yloc}px; left: ${coords.xloc}px`;
+
+      if (
+        coords.xloc + newLetter.offsetWidth >= pg.offsetWidth ||
+        coords.yloc + newLetter.offsetWidth >= pg.offsetHeight
+      ) {
+        setRandomLoc();
+        recursions++;
+      }
+    };
+    setRandomLoc();
+
+    const checkIntersections = () => {
+      if (recursions >= 100) return;
+
+      placedLettters.forEach((element) => {
+        if (elementsIntersect(newLetter, element)) {
+          setRandomLoc();
+          checkIntersections();
+          recursions++;
+        }
+      });
+    };
+    checkIntersections();
+
+    placedLettters.add(newLetter);
+  });
+};
+
+setupGame();
